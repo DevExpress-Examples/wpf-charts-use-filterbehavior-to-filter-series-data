18b0b2d1488ed3e4d5ffedf545f0971569b5ea88
Kseniya Kuzmina <kuzminaksyushka@gmail.com>
2022-08-08 10:39:25 +0400

    replace FilteringBehavior with FilterBehavior

--- CS/ChartFiltering/MainWindow.xaml.cs
+++ CS/ChartFiltering/MainWindow.xaml.cs
@@ -1,16 +1,13 @@
-﻿using System;
+﻿using DevExpress.Xpf.Core;
+using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Windows;
 using System.Windows.Resources;
 using System.Xml.Linq;
-using DevExpress.Utils.Filtering;
-using DevExpress.Xpf.Core;
 
 namespace ChartFiltering {
     public partial class MainWindow : ThemedWindow {
-        #region #MainWindow_Ctor
         public MainWindow() {
             InitializeComponent();
 
@@ -18,34 +15,15 @@ namespace ChartFiltering {
             XmlGdpValueProvider valueProvider = new XmlGdpValueProvider(dataStreamInfo.Stream);
             DataContext = new MainViewModel(valueProvider);
         }
-        #endregion #MainWindow_Ctor
     }
 
-    #region #MainViewModel
     public class MainViewModel {
         public IEnumerable<GdpValue> GdpValues { get; private set; }
 
-        //Filter parameters
-        public double MinGdpValue { get; private set; }
-        public double MaxGdpValue { get; private set; }
-        public int StartYear { get; private set; }
-        public int EndYear { get; private set; }
-        public IEnumerable<string> CountryNames { get; private set; }
-        public IEnumerable<string> ContinentNames { get; private set; }
-
-
         public MainViewModel(XmlGdpValueProvider valueProvider) {
             GdpValues = valueProvider.GetValues();
-            MinGdpValue = GdpValues.Min(gdp => gdp.Value);
-            MaxGdpValue = GdpValues.Max(gdp => gdp.Value);
-            StartYear = GdpValues.Min(gdp => gdp.Year);
-            EndYear = GdpValues.Max(gdp => gdp.Year);
-            CountryNames = GdpValues.Select(gdp => gdp.CountryName);
-            ContinentNames = GdpValues.Select(gdp => gdp.ContinentName);
-
         }
     }
-    #endregion #MainViewModel
 
     public class XmlGdpValueProvider {
         Stream xmlStream;
@@ -57,11 +35,11 @@ namespace ChartFiltering {
         public IEnumerable<GdpValue> GetValues() {
             XElement dataSet = XDocument.Load(xmlStream).Element("data-set");
             List<GdpValue> values = new List<GdpValue>();
-            foreach(XElement continent in dataSet.Elements("ContinentInfo")) {
+            foreach (XElement continent in dataSet.Elements("ContinentInfo")) {
                 String continentName = continent.Element("ContinentName").Value;
-                foreach(XElement country in continent.Element("Countries").Elements("CountryInfo")) {
+                foreach (XElement country in continent.Element("Countries").Elements("CountryInfo")) {
                     String countryName = country.Element("Name").Value;
-                    foreach(XElement gdpByYear in country.Element("Statistic").Elements("GDPByYear")) {
+                    foreach (XElement gdpByYear in country.Element("Statistic").Elements("GDPByYear")) {
                         int year = int.Parse(gdpByYear.Element("Year").Value);
                         double value = String.IsNullOrEmpty(gdpByYear.Element("GDP").Value)
                             ? 0
@@ -79,20 +57,10 @@ namespace ChartFiltering {
             return values;
         }
     }
-
-    #region #GdpValue
     public class GdpValue {
-        [FilterLookup(DataSourceMember = "ContinentNames", UseBlanks = false)]
         public string ContinentName { get; set; }
-
-        [FilterLookup(DataSourceMember = "CountryNames", UseBlanks = false)]
         public string CountryName { get; set; }
-
-        [FilterRange(MaximumMember = "MaxGdpValue", MinimumMember = "MinGdpValue")]
         public double Value { get; set; }
-
-        [FilterRange(MinimumMember = "StartYear", MaximumMember = "EndYear")]
         public int Year { get; set; }
     }
-    #endregion #GdpValue
 }
